52ea99015b3ae14011bc2f6e4f16276aedd0ce4b
luobochuanqi <luobochuanqi@qq.com>
2025-03-08 11:44:35 +0800

    implement command handling and GUI interaction for CheckPoints plugin

--- src/main/java/xyz/luobo/checkPoints/CheckPoints.java
+++ src/main/java/xyz/luobo/checkPoints/CheckPoints.java
@@ -1,15 +1,26 @@
 package xyz.luobo.checkPoints;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
 
 public final class CheckPoints extends JavaPlugin {
 
+    public static JavaPlugin instance;
+    public Logger logger = this.getLogger();
+
     @Override
     public void onEnable() {
         // Plugin startup logic
-        Logger logger = this.getLogger();
+        instance = this;
+
+        if (Bukkit.getPluginCommand("checkpoints") != null) {
+            Bukkit.getPluginCommand("checkpoints").setExecutor(new CheckPointsCmd());
+        }
+
+        Bukkit.getPluginManager().registerEvents(new GUIListener(), this);
+
         logger.info("CheckPoints is enabled!");
     }
 
